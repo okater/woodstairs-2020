ef78e0390b16c34164bbd60e348de2e10b4d43a4
okater <okater@gmail.com>
2020-03-19 15:25:59 +0200

    Basic finish - not adaptive

--- gulpfile.js
+++ gulpfile.js
@@ -11,16 +11,12 @@ function styles() {
     return (
         gulp
             .src('sass/main.scss')
-            // Initialize sourcemaps before compilation starts
             .pipe(sourcemaps.init())
             .pipe(sass())
             .on('error', sass.logError)
-            // Use postcss with autoprefixer and compress the compiled file using cssnano
-            .pipe(postcss([autoprefixer(), cssnano()]))
-            // Now add/write the sourcemaps
-            .pipe(sourcemaps.write())
+            //.pipe(postcss([autoprefixer(), cssnano()]))
+            //.pipe(sourcemaps.write())
             .pipe(gulp.dest('css'))
-            // Add browsersync stream pipe after compilation
             .pipe(browserSync.stream())
     );
 }
@@ -38,19 +34,12 @@ function reload() {
 function watch() {
     styles();
     browserSync.init({
-        // You can tell browserSync to use this directory and serve it as a mini-server
         server: {
             baseDir: "./"
         }
-        // If you are already serving your website locally using something like apache
-        // You can use the proxy setting to proxy that instead
-        // proxy: "yourlocal.dev"
     });
     gulp.watch('sass/**/*.scss', styles);
-    // We should tell gulp which files to watch to trigger the reload
-    // This can be html or whatever you're using to develop your website
-    // Note -- you can obviously add the path to the Paths object
-    gulp.watch('*.html', reload);
+    //gulp.watch('index.html', reload);
 }
 
 
